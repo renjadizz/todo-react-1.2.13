50bd05d563e06304e016e0d5f0d295d58b115267
Murat <renja.dizz@gmail.com>
2023-11-08 23:54:51 +0600

    add Task component

---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+
+
+const Task = () => {
+    return (
+        <label>
+            <span className="description">Completed task</span>
+            <span className="created">created 17 seconds ago</span>
+        </label>
+    );
+}
+
+
+export default Task;
